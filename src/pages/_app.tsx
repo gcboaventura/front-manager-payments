03305550780be54147895c962b5d0a98dc95ee52
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-26 16:17:48 -0300

    feaT: configuration head html

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<title>Pagamentos</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App
