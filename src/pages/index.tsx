5b4904ec96477a10d5ee893c584e6baabeb8f1f6
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-26 10:42:34 -0300

    refactor: project restructuring

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Payment } from '../sections'
+
+const Main: FC = (): JSX.Element => {
+  return (
+    <>
+      <Payment />
+    </>
+  )
+}
+export default Main
