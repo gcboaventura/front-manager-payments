e737c9a29ff0d55421da01c7522ba39e356fa0c0
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-26 21:51:07 -0300

    feat: import js bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../style/global.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
+	useEffect(() => {
+		require('bootstrap/dist/js/bootstrap.bundle.min.js')
+	}, [])
+
 	return (
 		<>
 			<Head>
