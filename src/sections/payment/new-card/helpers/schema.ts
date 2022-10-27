57b4857acdfaec06a3e9453af0c85c97c0d3b996
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-27 17:06:12 -0300

    feat: check card credit number with credit card utils

--- src/sections/payment/new-card/helpers/schema.ts
+++ src/sections/payment/new-card/helpers/schema.ts
@@ -1,12 +1,24 @@
 import { CreditCardForm, PaypalForm } from './types'
+import { CreditCardUtils } from '../../../../utils'
 import * as Yup from 'yup'
+
+const cardValidator = new CreditCardUtils()
 
 export const schemaPayPal: Yup.SchemaOf<PaypalForm> = Yup.object().shape({
 	paypal_email: Yup.string().required('Obrigatório')
 })
 
 export const schemaCreditCard: Yup.SchemaOf<CreditCardForm> = Yup.object().shape({
-	card_number: Yup.string().required('Obrigatório'),
+	card_number: Yup.string()
+		.required('Obrigatório')
+		.test('card_number', 'Cartão inválido', function (value: string | undefined): boolean {
+			const cardNumber = value?.replaceAll(' ', '')
+			if (cardNumber) {
+				const check = cardValidator.checkNumber(cardNumber)
+				return check
+			}
+			return false
+		}),
 	cvv: Yup.string().required('Obrigatório'),
 	validate: Yup.string().required('Obrigatório')
 })
