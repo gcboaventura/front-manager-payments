06c7d363c0ef2aed44b29ce9fb1e0f3b1e620d0e
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-27 16:20:39 -0300

    feat: mask input

---
+++ src/components/ui/mask-input/index.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react'
+import { Props } from './types'
+import { useField } from 'formik'
+import { Form } from 'react-bootstrap'
+import { Label } from '../label'
+import { ErrorMessage } from '../error-message'
+import { IMaskInput } from 'react-imask'
+import style from './maskinput.module.css'
+
+export const MaskInput: FC<Props> = ({
+	width,
+	variant,
+	error,
+	label,
+	icon,
+	name,
+	required,
+	mask,
+	...props
+}): JSX.Element => {
+	const [field, meta] = useField(name || 'name')
+
+	const hasError = !!(meta.touched && meta.error)
+
+	return (
+		<div>
+			{label && <Label error={hasError} children={label} />}
+			<div className={style.wrapper}>
+				<Form.Control
+					as={IMaskInput}
+					mask={mask}
+					className={hasError ? `${style.error} ${props.className}` : props.className}
+					required={required}
+					{...field}
+					{...props}
+				/>
+				{icon && icon}
+			</div>
+			{hasError && <ErrorMessage children={meta.error || 'Erro'} error={hasError} />}
+		</div>
+	)
+}
---
+++ src/components/ui/mask-input/types.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import { FormControlProps } from 'react-bootstrap'
+
+export type Variant = 'error'
+
+export interface Props extends FormControlProps {
+	variant?: Variant
+	width?: string | number
+	error?: boolean
+	label?: string
+	icon?: ReactNode
+	name: string
+	required?: boolean
+	mask: string
+}
