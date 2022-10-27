3055703a8d766d599162377fc4bc72b1f981bceb
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-27 15:18:11 -0300

    refactor: custom font size error message

--- src/components/ui/error-message/index.tsx
+++ src/components/ui/error-message/index.tsx
@@ -4,8 +4,8 @@ import style from './errormessage.module.css'
 
 export const ErrorMessage: FC<Props> = ({ children, error, ...props }): JSX.Element => {
 	return (
-		<span className={error ? `${style.error} ${props.className}` : props.className} {...props}>
+		<p className={error ? `${style.error} ${props.className}` : props.className} {...props}>
 			{children}
-		</span>
+		</p>
 	)
 }
