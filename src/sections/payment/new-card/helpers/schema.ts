2649b8754a4fbe35f347b60b4d33a1a0bf9279e1
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-27 17:24:34 -0300

    feat: yup check email

--- src/sections/payment/new-card/helpers/schema.ts
+++ src/sections/payment/new-card/helpers/schema.ts
@@ -1,11 +1,21 @@
 import { CreditCardForm, PaypalForm } from './types'
-import { CreditCardUtils } from '../../../../utils'
+import { CreditCardUtils, EmailUtils } from '../../../../utils'
 import * as Yup from 'yup'
 
 const cardValidator = new CreditCardUtils()
 
+const emailValidator = new EmailUtils()
+
 export const schemaPayPal: Yup.SchemaOf<PaypalForm> = Yup.object().shape({
-	paypal_email: Yup.string().required('Obrigatório')
+	paypal_email: Yup.string()
+		.required('Obrigatório')
+		.test('paypal_email', 'email inválido', function (value: string | undefined) {
+			if (value) {
+				const check = emailValidator.validator(value)
+				return check
+			}
+			return false
+		})
 })
 
 export const schemaCreditCard: Yup.SchemaOf<CreditCardForm> = Yup.object().shape({
