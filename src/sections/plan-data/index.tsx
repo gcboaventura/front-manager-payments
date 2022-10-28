2a8213aa5b5c1621bd096cec425b9bf456a86305
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-28 18:25:03 -0300

    feat: plan data

---
+++ src/sections/plan-data/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import { Row } from 'react-bootstrap'
+import { CurrentCard } from './current-card'
+import { CurrentPlan } from './current-plan'
+import { PlanInformation } from './plan-information'
+import style from './plandata.module.css'
+
+export const PlanData: FC = (): JSX.Element => {
+	return (
+		<section className={style.section}>
+			<div className="container pt-5">
+				<Row className="col-md-12 pt-5 pb-5">
+					<img className={style.logo} src="/img/logo-primage-white.png" />
+				</Row>
+
+				<Row className="align-items-center justify-content-between">
+					<div className="col-md-6">
+						<CurrentPlan />
+					</div>
+					<div className="col-md-6">
+						<CurrentCard />
+					</div>
+				</Row>
+				<div className="col-md-12">
+					<PlanInformation />
+				</div>
+			</div>
+		</section>
+	)
+}
