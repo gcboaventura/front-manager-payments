03606098f3dc03a7aa41daab3ea15d50bf43db06
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-28 17:59:18 -0300

    feat: add logo

--- src/sections/payment/index.tsx
+++ src/sections/payment/index.tsx
@@ -2,15 +2,11 @@ import { FC } from 'react'
 import { Row } from 'react-bootstrap'
 import { Invoices } from './invoices'
 import { NewPaymentOption } from './new-payment-option'
-import style from './payment.module.css'
 
 export const Payment: FC = (): JSX.Element => {
 	return (
-		<section className={style.payment}>
+		<div>
 			<div className="container">
-				<Row className="col-md-12 pt-5 pb-5">
-					<h1>Logo</h1>
-				</Row>
 				<Row className="align-items-start justify-content-between">
 					<div className="col-md-6">
 						<NewPaymentOption />
@@ -21,6 +17,6 @@ export const Payment: FC = (): JSX.Element => {
 					</div>
 				</Row>
 			</div>
-		</section>
+		</div>
 	)
 }
