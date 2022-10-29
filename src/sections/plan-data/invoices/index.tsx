cd9fec6f9dd8c5aa31f75cbca051ca3a95786a05
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-29 16:15:18 -0300

    feat: invoice list

--- src/sections/plan-data/invoices/index.tsx
+++ src/sections/plan-data/invoices/index.tsx
@@ -1,9 +1,99 @@
 import { FC } from 'react'
+import { File, Title } from '@/components'
+import { DateUtils } from '@/utils'
+import style from './invoices.module.css'
 
 export const Invoices: FC = (): JSX.Element => {
+	const dateHelper = new DateUtils()
+
+	interface Item {
+		date: string
+		status: string
+		link: string
+	}
+
+	const items: Item[] = [
+		{
+			date: dateHelper.toIso('01-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('02-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('03-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('04-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('05-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('06-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('07-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('08-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('09-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Fechada'
+		},
+		{
+			date: dateHelper.toIso('10-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Aberta'
+		},
+		{
+			date: dateHelper.toIso('11-10-2022'),
+			link: 'sfhvjklsdf',
+			status: 'Aberta'
+		}
+	]
+
 	return (
-		<div>
-			<h1>Faturas</h1>
-		</div>
+		<>
+			<Title className="h4">Faturas</Title>
+			{items.map((x: Item, index: number) => (
+				<div
+					key={index}
+					className="d-flex align-items-center justify-content-between shadow-sm rounded-3 p-3 mt-3 mb-3 bg-white"
+				>
+					<div className="col-md-6">
+						<span className="text-dark">{dateHelper.formatDateString(x.date)}</span>
+					</div>
+					<div className="col-md-4 text-center">
+						<span className={style[x.status === 'Fechada' ? 'closed' : 'open']}>{x.status}</span>
+					</div>
+					<div className="col-md-2 text-center">
+						<span className={style.wrapperLink}>
+							<File />
+							<a download={x.link} className={style.link}>
+								Baixar
+							</a>
+						</span>
+					</div>
+				</div>
+			))}
+		</>
 	)
 }
