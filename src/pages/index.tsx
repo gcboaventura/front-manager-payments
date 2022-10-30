c87ea4f9b2f22d9abe8ebe9a2cacbd24e78ecc2f
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-30 01:10:13 -0300

    refactor: update time out alert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,12 @@ const Main: FC = (): JSX.Element => {
 
 	return (
 		<>
-			<Alert show={alert.show} message={alert.message} variant={alert.variant} time={alert.time} />
+			<Alert
+				show={alert.show}
+				message={alert.message}
+				variant={alert.variant}
+				time={alert.time || 5000}
+			/>
 			<PlanData />
 			{/* <Payment /> */}
 		</>
