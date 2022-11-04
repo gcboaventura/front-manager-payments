fb44d8158d9ae87ebf3da3ab23175d942b83b2e9
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-04 15:35:19 -0300

    refactor: update routes plan

--- src/data/api/plan.ts
+++ src/data/api/plan.ts
@@ -14,12 +14,12 @@ interface AccountApiModel {
 
 export class PlanAPI implements AccountApiModel {
 	async getAll(data: RequestGetAllPlans): Promise<HttpResponse<ResponseGetAllPlans>> {
-		const url = '/get-all-plans'
+		const url = '/list-plan'
 		return Http.get(url)
 	}
 
 	async update(data: RequestUpdatePlan): Promise<HttpResponse<ResponseUpdatePlan>> {
-		const url = `/update-plan/${data.plan}`
-		return Http.get(url)
+		const url = `/update-plan`
+		return Http.post(url, data.id)
 	}
 }
