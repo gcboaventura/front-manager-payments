538419ff3645811cfe456f77b52049fce75b2b6f
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-05 17:48:17 -0300

    refactor: headers axios

--- src/data/config/http.ts
+++ src/data/config/http.ts
@@ -1,9 +1,9 @@
 import axios, { Axios } from 'axios'
-
 import { IncomingHttpHeaders } from 'http'
 
 const headers: IncomingHttpHeaders = {
-	'content-type': 'application/json'
+	'Content-Type': 'application/json; charset=UTF-8',
+	'Access-Control-Allow-Origin': '*'
 }
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL
