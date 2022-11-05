b123e52c5c7c1f361ae65cae2ea5775ff89608b2
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-05 15:42:44 -0300

    feat: cep model

---
+++ src/models/cep.ts
@@ -0,0 +1,16 @@
+export interface RequestGetCep {
+	cep: string
+}
+
+export interface ResponseGetCep {
+	cep: string
+	logradouro: string
+	complemento: string
+	bairro: string
+	localidade: string
+	uf: string
+	ibge: string
+	gia: string
+	ddd: string
+	siafi: string
+}
